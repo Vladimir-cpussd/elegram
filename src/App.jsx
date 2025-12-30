13f79c310f4cdb50cdf012e45e2af85852ee148d
Vladimir <cygankovladimir@gmail.com>
2025-12-30 18:14:08 +0300

    sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-// В основном файле маршрутизации
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Autorization from './pages/autorization/autorization';
-import Registration from './pages/registration/registration';
-import Chats from './pages/chats/chats';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Autorization from './pages/autorization/autorization'
+import Registration from './pages/registration/registration'
+import Restoration from './pages/restoration/restoration'
+import Chats from './pages/chats/chats'
+import './App.css'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Autorization />} />
-        <Route path="/chats" element={<Chats />} />
-      </Routes>
-    </Router>
-  );
+    <Routes>
+      <Route path="/" element={<Autorization />} />
+      <Route path="/registration" element={<Registration />} />
+      <Route path="/restoration" element={<Restoration />} />
+      <Route path="/chats" element={<Chats />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
